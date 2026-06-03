d5048dbbc1a72d51e2581fc7954ccea3776fb8ae
lombardi-g <gabriellomba@gmail.com>
2026-06-03 10:43:57 -0300

    routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { UpperBar } from './upper-bar/upper-bar';
 import { Footer } from './footer/footer';
 import { Content } from './content/content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UpperBar, Footer, Content],
+  imports: [UpperBar, Footer, Content],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
